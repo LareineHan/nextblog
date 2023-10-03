5d86f51011c1c5fa4a051e4b384ad01bb0d5d897
Lareine Han <lareine.han@gmail.com>
2023-10-02 19:11:06 -0700

    2nd commit

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-
-export async function getStaticProps() {
-	const allPostsData = getSortedPostsData();
-	return {
-		props: {
-			allPostsData,
-		},
-	};
-}
+import Link from 'next/link';
+import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
 	return (
@@ -34,15 +27,25 @@ export default function Home({ allPostsData }) {
 				<ul className={utilStyles.list}>
 					{allPostsData.map(({ id, date, title }) => (
 						<li className={utilStyles.listItem} key={id}>
-							{title}
+							<Link href={`/posts/${id}`}>{title}</Link>
 							<br />
-							{id}
-							<br />
-							{date}
+							<small className={utilStyles.lightText}>
+								<Date dateString={date} />
+							</small>
 						</li>
 					))}
 				</ul>
 			</section>
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	console.log(JSON.stringify(allPostsData, null, 2));
+
+	return {
+		props: {
+			allPostsData,
+		},
+	};
 }
